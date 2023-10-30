73b4af294a2ef0a457ea55629eb632b573066db7
Sayeh Jarollahi <sayeh@Sayehs-MacBook-Air.local>
2023-10-30 19:12:35 +0330

    create ListView for showing ads

--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import RedirectView
-from django.http import HttpResponseRedirect
+from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
 
 from .models.advertiser import Advertiser
 from .models.ad import Ad
@@ -8,40 +9,39 @@ from .models.attributes import Click, View
 from .forms import CreateAdForm
 
 
-def create_ad(request):
-    form = CreateAdForm(request.POST)
+class CreateAdFormView(FormView):
+    template_name = 'advertiser_management/create_ad.html'
+    form_class = CreateAdForm
+    success_url = '/advertiser-management/all-ads'
 
-    if form.is_valid():
-        advertiser_id = request.POST['advertiser_id']
+    def form_valid(self, form):
+        advertiser_id = form.cleaned_data['advertiser_id']
         try:
             advertiser = Advertiser.objects.get(pk=advertiser_id)
             img_url = form.cleaned_data['img_url']
-            link = request.POST['link']
-            title = request.POST['title']
-            ad = Ad.objects.create(advertiser=advertiser, title=title, img_url=img_url, link=link)
+            link = form.cleaned_data['link']
+            title = form.cleaned_data['title']
+            Ad.objects.create(advertiser=advertiser, title=title, img_url=img_url, link=link)
 
-            # bara sakhtesh view nabayad besaze
-            ip = request.ip
-            View.objects.create(ad=ad, ip=ip)
-
-            return HttpResponseRedirect('/advertiser-management/all-ads')
+            return super().form_valid(form)
 
         except(KeyError, Advertiser.DoesNotExist):
-            return render(request, 'advertiser_management/create_ad.html', {
+            return render(self.request, 'advertiser_management/create_ad.html', {
                 'form': form,
                 'error_message': 'Advertiser ID is not valid.'
             })
 
-    return render(request, 'advertiser_management/create_ad.html', {
-        'form': form
-    })
 
+class ShowAllAdsListView(ListView):
+    template_name = 'advertiser_management/ads.html'
+    model = Advertiser
 
-def show_ads_all(request):
-    advertisers = Advertiser.objects.all() #na intori
-    # update_all_advertisers(advertisers)
-    context = {'advertisers': advertisers}
-    return render(request, 'advertiser_management/ads.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        advertisers = Advertiser.objects.all()
+        update_advertisers_view(advertisers, self.request.ip)
+        context['advertisers'] = advertisers
+        return context
 
 
 class AdOnClickRedirectView(RedirectView):
@@ -51,5 +51,16 @@ class AdOnClickRedirectView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         ad = get_object_or_404(Ad, pk=kwargs['ad_id'])
-        # ad.update_on_click()
+        ip = self.request.ip
+        Click.objects.create(ad=ad, ip=ip)
         return ad.link
+
+
+def update_advertisers_view(advertisers, ip):
+    for advertiser in advertisers:
+        update_ads(advertiser, ip)
+
+
+def update_ads(advertiser, ip):
+    for ad in advertiser.ads.all():
+        View.objects.create(ad=ad, ip=ip)
